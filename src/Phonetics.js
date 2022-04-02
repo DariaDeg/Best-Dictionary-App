3254b27247f23f7315ed7f78eaa3d7922d25aa65
DariaDeg <daria.degtyarenko@exactprosystems.com>
2022-04-02 21:54:11 +0300

    CSS Part 2

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,14 +1,13 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  console.log(props.phonetics);
   return (
     <div className="Phonetics">
       <a href={props.phonetics.audio} target="_blank" rel="noopener noreferrer">
         Listen
       </a>
-      <br />
-      {props.phonetics.text}
+      <span className="respelling">{props.phonetics.text}</span>
     </div>
   );
 }
