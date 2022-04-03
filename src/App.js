79cbe846c16964f60f1517e234b85becd6476f9d
DariaDeg <daria.degtyarenko@exactprosystems.com>
2022-04-03 15:55:32 +0300

    background added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Dictionary from "./Dictionary";
-import logo from "./logo.jpg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -8,9 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-img-fluid" alt="logo" />
-        </header>
+        <header className="App-header"></header>
         <main>
           <Dictionary defaultKeyword="word" />
         </main>
